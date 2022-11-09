ba897ba13660ed0af5d49f0ace506dac9d2eed63
sirlollipop <mangafreakanime@hotmail.com>
2022-11-09 15:26:58 +0200

    Update Quiz.php

--- Quiz.php
+++ Quiz.php
@@ -6,12 +6,158 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<title>Quiz</title>
+	<style>
+    body{
+        padding-top: 100px;
+        padding-bottom: 100px;
+    }
+	</style>
 </head>
 <body>
 <?php
  
  ?>
-	<nav class="navbar navbar-dark bg-primary">
+	<!-- Navigointi palkki sivun yläosassa jossa Techvet logo -->
+	<nav class="navbar fixed-top navbar-dark bg-primary">
 	 <a class="navbar-brand" href="#"><img src="NextSteps_at_TechVET_logo.png" width="100" class="d-inline-block align-top" alt=""></a>
 	</nav>
+	
+	<!-- Navigointi palkki pohjalla jossa Erasmus logo -->
+	<nav class="navbar fixed-bottom navbar-dark bg-primary">
+	 <a class="navbar-brand" href="#"><img src="logosbeneficaireserasmusleft_en_0.jpg" width="200" class="d-inline-block align-top" alt=""></a>
+	</nav>
+	
+	<div class="container">
+	
+		<div class="card">
+		<form action="#">
+		<div class= "card-body">
+			
+				<p class="question">Are you okay?</p>
+				
+				<input type="radio" id="yes" name="question1" value="Yes">
+		  		<label for="yes">Yes</label>
+
+				<input type="radio" id="no" name="question1" value="No">
+		  		<label for="no">No</label>
+			
+		</div>
+		<div class= "card-body">
+			
+				<p class="card-text">Are you good?</p>
+				
+				<input type="radio" id="yes" name="question2" value="Yes">
+		  		<label for="yes">Yes</label>
+
+				<input type="radio" id="no" name="question2" value="No">
+		  		<label for="no">No</label>
+			
+		</div>
+		<div class= "card-body">
+			
+				<p class="card-text">Are you good?</p>
+				
+				<input type="radio" id="yes" name="question2" value="Yes">
+		  		<label for="yes">Yes</label>
+
+				<input type="radio" id="no" name="question2" value="No">
+		  		<label for="no">No</label>
+			
+		</div>
+		<div class= "card-body">
+			
+				<p class="card-text">Are you good?</p>
+				
+				<input type="radio" id="yes" name="question2" value="Yes">
+		  		<label for="yes">Yes</label>
+
+				<input type="radio" id="no" name="question2" value="No">
+		  		<label for="no">No</label>
+			
+		</div>
+		<div class= "card-body">
+			
+				<p class="card-text">Are you good?</p>
+				
+				<input type="radio" id="yes" name="question2" value="Yes">
+		  		<label for="yes">Yes</label>
+
+				<input type="radio" id="no" name="question2" value="No">
+		  		<label for="no">No</label>
+			
+		</div>
+		<div class= "card-body">
+			
+				<p class="card-text">Are you good?</p>
+				
+				<input type="radio" id="yes" name="question2" value="Yes">
+		  		<label for="yes">Yes</label>
+
+				<input type="radio" id="no" name="question2" value="No">
+		  		<label for="no">No</label>
+			
+		</div>
+		<div class= "card-body">
+			
+				<p class="card-text">Are you good?</p>
+				
+				<input type="radio" id="yes" name="question2" value="Yes">
+		  		<label for="yes">Yes</label>
+
+				<input type="radio" id="no" name="question2" value="No">
+		  		<label for="no">No</label>
+			
+		</div>
+		<div class= "card-body">
+			
+				<p class="card-text">Are you good?</p>
+				
+				<input type="radio" id="yes" name="question2" value="Yes">
+		  		<label for="yes">Yes</label>
+
+				<input type="radio" id="no" name="question2" value="No">
+		  		<label for="no">No</label>
+			
+		</div>
+		<div class= "card-body">
+			
+				<p class="card-text">Are you good?</p>
+				
+				<input type="radio" id="yes" name="question2" value="Yes">
+		  		<label for="yes">Yes</label>
+
+				<input type="radio" id="no" name="question2" value="No">
+		  		<label for="no">No</label>
+			
+		</div>
+		<div class= "card-body">
+			
+				<p class="card-text">Are you good?</p>
+				
+				<input type="radio" id="yes" name="question2" value="Yes">
+		  		<label for="yes">Yes</label>
+
+				<input type="radio" id="no" name="question2" value="No">
+		  		<label for="no">No</label>
+			
+		</div>
+		<div class= "card-body">
+			
+				<p class="card-text">Are you good?</p>
+				
+				<input type="radio" id="yes" name="question2" value="Yes">
+		  		<label for="yes">Yes</label>
+
+				<input type="radio" id="no" name="question2" value="No">
+		  		<label for="no">No</label>
+			
+		</div>
+		</form>	
+		</div>
+	
+	</div>
+	
+
+	
 </body>
+</html>

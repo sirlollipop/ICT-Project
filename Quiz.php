f61c680c355585339a7b4df917830c7ed8697b87
bh720 <59826982+bh720@users.noreply.github.com>
2022-11-18 17:58:06 +0200

    Add files via upload

--- Quiz.php
+++ Quiz.php
@@ -6,6 +6,9 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<link rel="stylesheet" href="styles.css">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	<title>Quiz</title>
 	
 </head>
@@ -106,17 +109,37 @@
 				<label for="yes">Completely agree</label>
 			
 		</div>
-	<nav aria-label="Page navigation example">
-  		<ul class="pagination justify-content-end">
-    		<li class="page-item">
-      	<a class="page-link" href="quiz2.php">Submit</a>
-    		</li>
-  		</ul>
-	</nav>
-	</div>
-		</form>
-		</div>
-	</div>
+<!-- Button trigger modal -->
+<div class="text-right">
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#submitModal">
+  Submit
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="submitModal" tabindex="-1" role="dialog" aria-labelledby="submitModal" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="submitModal">Your points</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        placeholder
+      </div>
+      <div class="modal-footer">
+	  	<a class="btn btn-primary" href="quiz2.php" role="button">Next Page</a>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+</div>
+</form>
+</div>
+</div>
 
 </body>
 </html>

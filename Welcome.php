d0bc6c80c6ad5196dd97c717546a20c495f7e18f
sirlollipop <mangafreakanime@hotmail.com>
2022-12-05 10:36:08 +0200

    Finished with all the data from all 4 categories

--- Welcome.php
+++ Welcome.php
@@ -23,6 +23,15 @@ $user_result =mysqli_query($conn, $user_num);
 $avg_xy = "SELECT AVG(score1), AVG(score2), AVG(score3), AVG(score4), AVG(score5), AVG(score6), AVG(score7), AVG(score8), AVG(score9), AVG(score10) FROM category1";
 $avg_result =mysqli_query($conn, $avg_xy);
 
+$avg_xy2 = "SELECT AVG(score1), AVG(score2), AVG(score3), AVG(score4), AVG(score5), AVG(score6), AVG(score7), AVG(score8), AVG(score9), AVG(score10) FROM category2";
+$avg_result2 =mysqli_query($conn, $avg_xy2);
+
+$avg_xy3 = "SELECT AVG(score1), AVG(score2), AVG(score3), AVG(score4), AVG(score5), AVG(score6), AVG(score7), AVG(score8), AVG(score9), AVG(score10) FROM category3";
+$avg_result3 =mysqli_query($conn, $avg_xy3);
+
+$avg_xy4 = "SELECT AVG(score1), AVG(score2), AVG(score3), AVG(score4), AVG(score5), AVG(score6), AVG(score7), AVG(score8), AVG(score9), AVG(score10) FROM category4";
+$avg_result4 =mysqli_query($conn, $avg_xy4);
+
 if ($result->num_rows > 0) {
   
   while($row = $result->fetch_assoc()) {
@@ -74,16 +83,16 @@ $conn->close();
 		<?php
 		while($row = $avg_result->fetch_assoc()) {
 			 //echo "Average score: "  . $row["AVG(score)"];
-			$score1 = $row["AVG(score1)"];
-			$score2 = $row["AVG(score2)"];
-			$score3 = $row["AVG(score3)"];
-			$score4 = $row["AVG(score4)"];
-			$score5 = $row["AVG(score5)"];
-			$score6 = $row["AVG(score6)"];
-			$score7 = $row["AVG(score7)"];
-			$score8 = $row["AVG(score8)"];
-			$score9 = $row["AVG(score9)"];
-			$score10 = $row["AVG(score10)"];
+			$score1_1 = $row["AVG(score1)"];
+			$score1_2 = $row["AVG(score2)"];
+			$score1_3 = $row["AVG(score3)"];
+			$score1_4 = $row["AVG(score4)"];
+			$score1_5 = $row["AVG(score5)"];
+			$score1_6 = $row["AVG(score6)"];
+			$score1_7 = $row["AVG(score7)"];
+			$score1_8 = $row["AVG(score8)"];
+			$score1_9 = $row["AVG(score9)"];
+			$score1_10 = $row["AVG(score10)"];
 
 			echo "<table class='table'> <tr>";
 			echo " <th>Category</th>";
@@ -94,17 +103,93 @@ $conn->close();
 			echo " <th>Execution</th>";
 			echo "</tr>";
 			echo "<tr><td> Substance </td>";
-			echo "<td>" . $score1 + $score2 . "</td>";
-			echo "<td>" . $score3 + $score4 . "</td>";
-			echo "<td>" . $score5 + $score6 . "</td>";
-			echo "<td>" . $score7 + $score8 . "</td>";
-			echo "<td>" . $score9 + $score10 . "</td> </tr>";
+			echo "<td>" . $score1_1 + $score1_2 . "</td>";
+			echo "<td>" . $score1_3 + $score1_4 . "</td>";
+			echo "<td>" . $score1_5 + $score1_6 . "</td>";
+			echo "<td>" . $score1_7 + $score1_8 . "</td>";
+			echo "<td>" . $score1_9 + $score1_10 . "</td> </tr>";
+			
+			$_SESSION["Score1"] = $row["AVG(score1)"];
+			$_SESSION["Score2"] = $row["AVG(score2)"]; 
+
+		}
+		
+		while($row = $avg_result2->fetch_assoc()) {
+			 //echo "Average score: "  . $row["AVG(score)"];
+			$score2_1 = $row["AVG(score1)"];
+			$score2_2 = $row["AVG(score2)"];
+			$score2_3 = $row["AVG(score3)"];
+			$score2_4 = $row["AVG(score4)"];
+			$score2_5 = $row["AVG(score5)"];
+			$score2_6 = $row["AVG(score6)"];
+			$score2_7 = $row["AVG(score7)"];
+			$score2_8 = $row["AVG(score8)"];
+			$score2_9 = $row["AVG(score9)"];
+			$score2_10 = $row["AVG(score10)"];
+
+			echo "<tr><td> Future </td>";
+			echo "<td>" . $score2_1 + $score2_2 . "</td>";
+			echo "<td>" . $score2_3 + $score2_4 . "</td>";
+			echo "<td>" . $score2_5 + $score2_6 . "</td>";
+			echo "<td>" . $score2_7 + $score2_8 . "</td>";
+			echo "<td>" . $score2_9 + $score2_10 . "</td> </tr>";
+			
+			$_SESSION["Score1"] = $row["AVG(score1)"];
+			$_SESSION["Score2"] = $row["AVG(score2)"]; 
+
+		}
+		
+		while($row = $avg_result3->fetch_assoc()) {
+			 //echo "Average score: "  . $row["AVG(score)"];
+			$score3_1 = $row["AVG(score1)"];
+			$score3_2 = $row["AVG(score2)"];
+			$score3_3 = $row["AVG(score3)"];
+			$score3_4 = $row["AVG(score4)"];
+			$score3_5 = $row["AVG(score5)"];
+			$score3_6 = $row["AVG(score6)"];
+			$score3_7 = $row["AVG(score7)"];
+			$score3_8 = $row["AVG(score8)"];
+			$score3_9 = $row["AVG(score9)"];
+			$score3_10 = $row["AVG(score10)"];
+
+			echo "<tr><td> Studification </td>";
+			echo "<td>" . $score3_1 + $score3_2 . "</td>";
+			echo "<td>" . $score3_3 + $score3_4 . "</td>";
+			echo "<td>" . $score3_5 + $score3_6 . "</td>";
+			echo "<td>" . $score3_7 + $score3_8 . "</td>";
+			echo "<td>" . $score3_9 + $score3_10 . "</td> </tr>";
+
+			$_SESSION["Score1"] = $row["AVG(score1)"];
+			$_SESSION["Score2"] = $row["AVG(score2)"]; 
+
+		}
+		
+		while($row = $avg_result4->fetch_assoc()) {
+			 //echo "Average score: "  . $row["AVG(score)"];
+			$score4_1 = $row["AVG(score1)"];
+			$score4_2 = $row["AVG(score2)"];
+			$score4_3 = $row["AVG(score3)"];
+			$score4_4 = $row["AVG(score4)"];
+			$score4_5 = $row["AVG(score5)"];
+			$score4_6 = $row["AVG(score6)"];
+			$score4_7 = $row["AVG(score7)"];
+			$score4_8 = $row["AVG(score8)"];
+			$score4_9 = $row["AVG(score9)"];
+			$score4_10 = $row["AVG(score10)"];
+
+			echo "<tr><td> Guidance </td>";
+			echo "<td>" . $score4_1 + $score4_2 . "</td>";
+			echo "<td>" . $score4_3 + $score4_4 . "</td>";
+			echo "<td>" . $score4_5 + $score4_6 . "</td>";
+			echo "<td>" . $score4_7 + $score4_8 . "</td>";
+			echo "<td>" . $score4_9 + $score4_10 . "</td> </tr>";
 			echo "</table>";
 			
 			$_SESSION["Score1"] = $row["AVG(score1)"];
 			$_SESSION["Score2"] = $row["AVG(score2)"]; 
 
 		}
+		
 		?>
         <div class="row">
             <div class="col-6 d-flex justify-content-center">
